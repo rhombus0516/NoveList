5b62c7bf8a914fc0014a7b102736fe3b2cda02fe
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-08 01:06:53 +0000

    [Update]ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :customers
-  #devise_for :admins
 
   # 顧客用
   # URL /customers/sign_in ...
@@ -14,5 +12,8 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  #root to: "homes#top"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
