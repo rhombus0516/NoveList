ab03be2381ddf6d625a8a1a1ae39b679a952411e
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-08 02:41:38 +0000

    [Update]user側homes top記述,root編集

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
-  #root to: "homes#top"
-  
+    scope module: :user do
+    root to: "homes#top"
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
