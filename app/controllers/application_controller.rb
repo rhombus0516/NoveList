636da0666b6ef9eda3dfce38241bb8f2528babce
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-11 01:06:06 +0000

    [Add]ログアウト後のルート指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
         books_path
     end
     
+    def after_sigin_out_path_for(resource)
+        root_path
+    end
+    
     protected
     
     def configure_permitted_parameters
