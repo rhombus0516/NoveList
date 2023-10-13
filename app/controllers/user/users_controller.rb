5c8a17b87d48248e07681b39e485a8b0d4b43196
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-13 01:40:13 +0000

    [Update]他のユーザ－からの編集権限

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,6 +1,7 @@
 class User::UsersController < ApplicationController
     before_action :authenticate_user!
     before_action :ensure_guest_user, only: [:edit]
+    before_action :is_matching_login_user, only: [:edit, :update]
     
     def show
         @user = User.find(params[:id])
@@ -12,9 +13,9 @@ class User::UsersController < ApplicationController
     end    
     
     def update
-        user = User.find(params[:id])
-        user.update(user_params)
-        redirect_to user_path(user)
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
     end
     
     def destroy
@@ -27,6 +28,13 @@ class User::UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:name, :profile_image)
+    end
+    
+    def is_matching_login_user
+        user = User.find(params[:id])
+        unless user.id == current_user.id
+          redirect_to books_path
+        end
     end
     
     def ensure_guest_user
