cc64c3409da0fea0897ab46fa0e568953b0f301e
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-19 01:57:04 +0000

    [Update]管理者側いいね非表示、フォロー機能非表示

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -44,7 +44,7 @@ class User::BooksController < ApplicationController
     def update
         @book = Book.find(params[:id])
         if @book.update(book_params)
-            redirect_to book_path(book.id)
+            redirect_to book_path(@book.id)
         else
             render :edit
         end
