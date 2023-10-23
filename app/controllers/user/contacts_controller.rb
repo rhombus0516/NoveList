a326098200fce8e62cc18e4f786aada08bc0944a
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-23 10:59:17 +0000

    [Add]お問い合わせ機能

--- app/controllers/user/contacts_controller.rb
+++ app/controllers/user/contacts_controller.rb
@@ -1,22 +1,23 @@
 class User::ContactsController < ApplicationController
-    
+
     def new
         @contact = Contact.new
     end
-    
+
     def confirm
         @contact = Contact.new(contact_params)
         if @contact.invalid?
           render :new
         end
     end
-    
+
     def back
         @contact = Contact.new(contact_params)
         render :new
     end
-    
+
     def create
+        
         @contact = Contact.new(contact_params)
         if @contact.save
           ContactMailer.send_mail(@contact).deliver_now
@@ -24,14 +25,14 @@ class User::ContactsController < ApplicationController
         else
           render :new
         end
-        
+    end    
+
     def done
     end
 
     private
-    
+
         def contact_params
-            params.require(:contact).permit(:email, :name, :subject, :message)             
+            params.require(:contact).permit(:email, :name, :message)
         end
-    end
 end
