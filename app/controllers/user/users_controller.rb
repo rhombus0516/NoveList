b9c7523f6b8eb44463c40d7b6c11ad1864c81a81
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-10-23 02:25:05 +0000

    [Add]フラッシュメッセージ、文字数バリデーション

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,74 +2,74 @@ class User::UsersController < ApplicationController
     #before_action :authenticate_user!
     before_action :ensure_guest_user, only: [:edit]
     before_action :is_matching_login_user, only: [:edit, :update]
-    
+
     def index
         @users = User.all
         @book = Book.new
         @books = Book.all
     end
-    
+
     def show
         @user = User.find(params[:id])
         @books = @user.book
         @following_users = @user.following_users
         @follower_users = @user.follower_users
     end
-    
+
     def edit
         @user = User.find(params[:id])
-    end    
-    
+    end
+
     def update
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "変更を保存しました"
             redirect_to user_path(@user)
         else
-            flash[:notice] = "保存に失敗しました"
+            flash.now[:notice] = "保存に失敗しました"
             render:edit
-        end    
+        end
     end
-    
+
     def destroy
         @user =User.find(params[:id])
         @user.destroy
         redirect_to root_path
     end
-    
+
     def follows
         user = User.find(params[:id])
         @user = user.following_users
     end
-    
+
     def followers
         user = User.find(params[:id])
         @user = user.follower_users
     end
-    
-        
+
+
     def liked_posts
         @liked_posts = Book.liked_posts(current_user)
         @books = current_user.books
-    end    
-    
+    end
+
       private
 
     def user_params
         params.require(:user).permit(:name, :profile_image, :introduction)
     end
-    
+
     def is_matching_login_user
         user = User.find(params[:id])
         unless user.id == current_user.id
           redirect_to books_path
         end
     end
-    
+
     def ensure_guest_user
         @user = User.find(params[:id])
         if @user.guest_user?
           redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
         end
-    end  
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,58 +3,60 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :book, dependent: :destroy
-  
+
+  validates :name, presence: true,length: { in: 1..10 }
+
   #ユーザー画像
   has_one_attached :profile_image
 
   #いいね
   has_many :favorites, dependent: :destroy
   has_many :books, through: :favorites
-  
+
   #コメント
   has_many :book_comments, dependent: :destroy
-  
+
   #フォロー
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  
+
   #フォロワー
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :following_users, through: :followers, source: :followed
   has_many :follower_users, through: :followeds, source: :follower
-  
+
   #フォローしたときの処理
   def follow(user_id)
     followers.create(followed_id: user_id)
   end
-  
+
   #フォローを外すときの処理
   def unfollow(user_id)
     followers.find_by(followed_id: user_id).destroy
   end
-  
+
   #フォローしているか確認
   def following?(user)
     following_users.include?(user)
   end
-  
+
   #ゲストログイン
-  
+
   GUEST_USER_EMAIL = "guest@example.com"
-  
+
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
-    end  
+    end
   end
-  
+
   def guest_user?
     email == GUEST_USER_EMAIL
   end
-  
+
   #ユーザー画像
   def get_profile_image(width,height)
     unless profile_image.attached?
@@ -63,20 +65,20 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
-  
+
   #検索機能
   def self.looks(search, word)
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
     elsif search == "forward_match"
       @user = User.where("name LIKE?", "#{word}%")
-    elsif search == "backward_match"  
+    elsif search == "backward_match"
       @user = User.where("name LIKE?", "%#{word}")
-    elsif search == "partial_match"    
+    elsif search == "partial_match"
       @user = User.where("name LIKE?", "%#{word}%")
     else
       @user = User.all
-    end  
-  end  
+    end
+  end
 end
 
