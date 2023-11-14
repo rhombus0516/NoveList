fb2a507dec55081dc28ea7c498b99d63531d9b16
rhombus0516 <karaya0913@gmail.com>
2023-11-14 06:13:25 +0000

    [Update]文字数のカウント

--- app/javascript/packs/count.js
+++ app/javascript/packs/count.js
@@ -2,9 +2,9 @@ function count (){
   const bookText = document.getElementById('book_text');
 
   bookText.addEventListener("keyup", () => {
-   let titleLength = bookText.value.length
+   let bookLength = bookText.value.length
    let countText = document.getElementById('count_text')
-   countText.innerHTML = `${titleLength}文字`
+   countText.innerHTML = `${bookLength}文字`
   });
 }
 window.addEventListener('load', count);
